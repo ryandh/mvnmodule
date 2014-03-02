0d2cfc66ab5783dc81ad95a4154e0a2258427a2a
RyanDH.Mac <androidyou2010@gmail.com>
2014-03-02 12:09:56 -0800

    init commit

---
+++ CSharpClient/src/main/java/CSharpClientApp.java
@@ -0,0 +1,8 @@
+public class CSharpClientApp {
+
+	public static void main(String[] args) {
+		System.out.println("CSharpClient " + CSharpLib.Foo());
+		//return "CSharpClient" + CSharpLib.Foo();
+	}
+
+}
---
+++ ModuleCSharp/src/main/java/CSharpLib.java
@@ -0,0 +1,6 @@
+public class CSharpLib {
+	public static String Foo() {
+		System.out.println("CSharpLIb");
+		return "CSahrpLib";
+	}
+}
